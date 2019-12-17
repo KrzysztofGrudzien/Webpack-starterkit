9aaa6645207cf77562a0477ff99a51016b808a2c
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-12-17 16:43:56 +0100

    build: set up init production configuration

--- config/webpack.config.production.js
+++ config/webpack.config.production.js
@@ -1,3 +1,12 @@
+const path = require('path');
+
 module.exports = {
-    mode: "production"
+    mode: 'production',
+    entry: {
+        build: './src/index.js'
+    },
+    output: {
+        filename: '[name].[contenthash:8].js',
+        path: path.resolve(__dirname, '../', 'dist')
+    }
 }

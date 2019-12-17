17905993fb4a17b88a054a60dd87624137d1c1c6
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-12-17 20:41:40 +0100

    feat: install plugin creating dynamic html file based on the template html file

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -11,6 +12,18 @@ module.exports = {
         path: path.resolve(__dirname, '../', 'dist')
     },
     plugins: [
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            'meta': {
+                'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
+                'charset': 'UTF-8',
+                'X-UA-Compatible': 'ie=edge',
+                'description': 'webpack starter-kit allowing all developers to build theirs projects',
+                'robots': 'index, follow',
+                'googlebot': 'index, follow'
+            },
+            'title': 'Webpack Starter-Kit',
+            template: 'src/template.html'
+        })
     ]
 }

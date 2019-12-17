de17fe26e846154c08c1a9e97f30cae0edb56d9b
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-12-17 16:26:47 +0100

    build: add input and output webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        build: './src/index.js'
+    },
+    output: {
+        filename: '[name].[contenthash:8].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}

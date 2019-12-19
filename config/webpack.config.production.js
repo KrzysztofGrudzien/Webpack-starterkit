1413caf07c3e48c1ddae2db5c088eb10ab3db2e8
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2019-12-19 19:39:25 +0100

    fix: rename mode value to production

--- config/webpack.config.production.js
+++ config/webpack.config.production.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         build: './src/index.js'
     },
